a3c2b0e93296562694e6c292a7c2c141b1fc1052
Your Name <yashshah2774@gmail.com>
2024-02-23 14:11:36 +0530

    Data Transformation Added

--- src/componenets/data_ingestion.py
+++ src/componenets/data_ingestion.py
@@ -1,3 +1,56 @@
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath('logging')))
+# from src.exception import CustomException
+# from src.logger import logging
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from dataclasses import dataclass
+# import logging
+# from src.componenets.data_transformation import DataTransformation, DataTransformationConfig
+#
+#
+# @dataclass
+# class DataIngestionConfig:
+#     train_data_path: str = os.path.join('artifacts', 'train.csv')
+#     test_data_path: str = os.path.join('artifacts', 'test.csv')
+#     raw_data_path: str = os.path.join('artifacts', 'data.csv')
+#
+#
+# class DataIngestion:
+#     def __init__(self):
+#         self.ingestion_config = DataIngestionConfig()
+#
+# def initiate_data_ingestion(self):
+#     logging.info("Entered the Data Ingestion Method/Componenet.!")
+#     try:
+#         # JUST CHANGE THIS CODE TO CONNECT WITH DB
+#         df = pd.read_csv('notebook/data/stud.csv')
+#         #
+#         logging.info('Read Dataset as Dataframe.!')
+#         directory = os.path.dirname(self.ingestion_config.train_data_path)
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
+#         df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+#         logging.info("Train Test Split Initiated.!")
+#         train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+#         train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+#         test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+#         logging.info('Ingestion of Data is Completed.!')
+#
+#         return (
+#             self.ingestion_config.train_data_path,
+#             self.ingestion_config.test_data_path
+#         )
+#     except Exception as e:
+#         raise CustomException(e, sys)
+#
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_transformation ()
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath('logging')))
@@ -7,7 +60,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 import logging
-
+from src.componenets.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -22,7 +75,7 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the Data Ingestion Method/Componenet.!")
+        logging.info("Entered the Data Ingestion Method/Component.!")
         try:
             # JUST CHANGE THIS CODE TO CONNECT WITH DB
             df = pd.read_csv('notebook/data/stud.csv')
@@ -47,4 +100,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_Data_transformation(train_data, test_data)
